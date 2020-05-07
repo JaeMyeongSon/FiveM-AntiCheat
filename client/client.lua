51245a7c2b6011c5e61f95b80e15602751edb6f4
JaeMyeongSon <bjwytm72@gmail.com>
2020-05-07 17:11:28 +0900

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,14 @@
 -- hi
+AddEventHandler(
+    "esx:getSharedObject",
+    function(cb)
+        if AlreadyTriggered == true then
+            CancelEvent()
+            cb(nil)
+            return
+        end
+        TriggerServerEvent('Gal:EsxObj')
+        AlreadyTriggered = true
+        cb(nil)
+    end
+)
